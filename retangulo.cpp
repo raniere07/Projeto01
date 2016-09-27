0da2be073ce14fbf7242a119e0cfc3776ec53eab
Victor Raniere <victoraniere@outlook.com>
2016-09-27 19:28:38 -0300

    update

--- retangulo.cpp
+++ retangulo.cpp
@@ -1,10 +1,42 @@
 #include "retangulo.h"
 #include "poligono.h"
 
-Retangulo::Retangulo(int x, int y, int largura, int altura)
+Retangulo::Retangulo(int x,int y, int altura, int largura)
 {
-    Ponto x0 = x; y0 = y;
-    getX = largura; getY = altura;
+   Ponto Aux;
+   x = Aux[0].getX();
+   y = Aux[0].getY();
+   int i;
+   for(i=0; i<3; i++)
+   {
+       if(Aux[i].getX()>=Aux[i+1].getX())
+       {
+           largura = Aux[i].getX();
+       }
+       else(Aux[i].getX()<=Aux[i+1].getX())
+       {
+           largura = Aux[i+1].getX();
+       }
+
+           if(Aux[i].getY()>=Aux[i+1].getY())
+           {
+              altura = Aux[i].getY();
+           }
+           else(Aux[i].getY()<=Aux[i+1].getY())
+           {
+               altura = Aux[i+1].getY();
+           }
+   }
+Retangulo::~Retangulo();
+Retangulo Retangulo::imprime(int x,int y, int altura, int largura)
+{
+    int i;
+    for(i=0; i<3; i++)
+    {
+        cout<< x << ","<< y+altura <<"|"<< x+largura<<","<<y+altura<<endl<<x<<","<<y<<"|"<<x+largura<<y<<endl;
+    }
 }
- Retangulo::~Retangulo(){}
+
+}
+
 
